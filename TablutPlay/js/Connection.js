26fec1bb581c16c14dfaa11487cca4c190d84b77
GithubMarc <marc.danjoux@imerir.com>
2016-01-05 10:45:27 +0100

    Remise de la requette http

--- TablutPlay/js/Connection.js
+++ TablutPlay/js/Connection.js
@@ -2,7 +2,8 @@
 
 function checkConnection() {
     var json = {"login":loginTextField.text, "password":passwordTextField.text};
-    //TablutClientScript.postHttpRequestServer(TablutClientScript.serverAddr, TablutClientScript.serverPath, TablutClientScript.httpPort, json);
+    TablutClientScript.postHttpRequestServer(TablutClientScript.serverAddr, TablutClientScript.serverPath, TablutClientScript.httpPort, json);
+
     mainForm.playPage.wsClient.active = true;
 }
 
