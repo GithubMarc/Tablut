0d8fb007c00679522d358de6cb432e9d3f248329
GithubMarc <marc.danjoux@imerir.com>
2016-01-06 16:19:47 +0100

    integration de Piece.js et TablutClient.js dans le même fichier Jeu.js qui gère les fonctions du jeu, tant sur la partie graphique que l'execution des mouvements et les communications websockets avec le serveur

--- TablutPlay/js/Connection.js
+++ TablutPlay/js/Connection.js
@@ -1,11 +1,11 @@
-.import "TablutClient.js" as TablutClientScript
+.import "Jeu.js" as JeuScript
 
 function checkConnection() {
     var json = {"login":loginTextField.text, "password":passwordTextField.text};
-    TablutClientScript.postHttpRequestServer(TablutClientScript.serverAddr, TablutClientScript.serverPath, TablutClientScript.httpPort, json);
+    JeuScript.postHttpRequestServer(JeuScript.serverAddr, JeuScript.serverPath, JeuScript.httpPort, json);
 }
 
 function checkConnection2() {
-    TablutClientScript.getHttpRequestServer(TablutClientScript.serverAddr, TablutClientScript.socketServerPath, TablutClientScript.httpPort);
+    JeuScript.getHttpRequestServer(JeuScript.serverAddr, JeuScript.socketServerPath, JeuScript.httpPort);
 }
 
